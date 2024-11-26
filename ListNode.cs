d778e45c089a0f50b69ef25a92b876904fd3d1a5
Fanwei <2500097267@qq.com>
2024-11-26 10:19:34 +0100

    feat: add ListNode class

---
+++ ListNode.cs
@@ -0,0 +1,16 @@
+namespace leetcode;
+
+/// <summary>
+/// Definition for singlt-linked list.
+/// </summary>
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
